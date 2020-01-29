69cd22a18805aa35526029660765b720ad713952
Nicolas Kircher <nicolas.kircher5@etu.univ-lorraine.fr>
2020-01-29 15:32:21 +0100

    debug decrypt

--- commande_dev/server.js
+++ commande_dev/server.js
@@ -8,7 +8,8 @@ const CustomError = require('./class/Error');
 const Command = require('./class/Command');
 const sanitizeHtml = require('sanitize-html');
 const uid = require('uuid');
-const bcrypt = require('bcrypt');
+
+const bcrypt2 = require('bcrypt');
 
 const defaultSize = 5;
 
